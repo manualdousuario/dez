afafcfdf86004cfb83764d32c33d9b2236ebc5da
Rodrigo Ghedin <rodrigo@ghed.in>
2023-11-10 18:19:54 -0300

    Faltou este arquivo (enviei o template-tags.php por engano)

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -17,11 +17,6 @@ function dez_body_classes( $classes ) {
 		$classes[] = 'hfeed';
 	}
 
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
-
 	return $classes;
 }
 add_filter( 'body_class', 'dez_body_classes' );
