ecf6939820c08c99ec1c85c7d72339cd4aa81c00
Rodrigo Ghedin <rodrigo@ghed.in>
2023-11-10 18:11:48 -0300

    Remove sidebar/área de widgets

--- 404.php
+++ 404.php
@@ -32,5 +32,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
--- functions.php
+++ functions.php
@@ -99,26 +99,6 @@ function dez_content_width() {
 add_action( 'after_setup_theme', 'dez_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function dez_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'dez' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'dez' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'dez_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function dez_scripts() {
