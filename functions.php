75eee8265392c13cc25cbfb37ad1c1e5017245a3
Renan Bernordi <altendorfme@gmail.com>
2023-11-30 09:36:42 -0300

    remove os actions do guntenberg do tema para substituir por um plugin

--- functions.php
+++ functions.php
@@ -111,30 +111,6 @@ function dez_remove_large_image_sizes() {
 add_action( 'init', 'dez_remove_large_image_sizes' );
 
 /**
- * Desativa Gutemberg.
- */
-wp_dequeue_style( 'wp-block-library' );
-wp_deregister_style( 'wp-block-library' );
-
-wp_dequeue_style( 'wp-block-library-theme' );
-wp_deregister_style( 'wp-block-library-theme' );
-
-wp_dequeue_style( 'global-styles' );
-wp_deregister_style( 'global-styles' );
-
-wp_dequeue_style( 'classic-theme-styles-css' );
-wp_deregister_style( 'classic-theme-styles-css' );
-
-remove_action( 'wp_enqueue_scripts', 'wp_enqueue_classic_theme_styles' );
-remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
-remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
-
-add_filter( 'use_block_editor_for_post', '__return_false' );
-
-add_filter( 'use_widgets_block_editor', '__return_false' );
-add_filter( 'use_widgets_blog_editor', '__return_false' );
-
-/**
  * Desativa estilos e scripts de plugins.
  */
 add_action(
