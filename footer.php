9a30fcf4184a337dfeaaa701d587f8e479abb6a9
Rodrigo Ghedin <rodrigo@ghed.in>
2024-02-23 18:31:27 -0300

    Novo cabeçalho mais informativo
    * Altera logo e cabeçalho
    * Diminui margens entre elementos (leiaute mais denso)
    * Remove itens agora redundantes do rodapé
    * Altera cor do link “Continue lendo” (`<!--more-->`)
    
    Resolve #43

--- footer.php
+++ footer.php
@@ -12,15 +12,6 @@
 ?>
 
 <footer id="colophon" class="site-footer">
-	<div class="site-footer-info">
-		<p>O <strong>Manual do Usuário</strong> é um blog focado em tecnologia, criado e mantido por Rodrigo Ghedin. <a href="/sobre">Saiba mais</a>.</p>
-		<p>Inscreva-se na newsletter (e/ou assine o <a href="https://manualdousuario.net/feed/" class="rss-manual">feed RSS</a>):</p>
-
-		<form action="https://sendy.manualdousuario.net/subscribe" method="post" accept-charset="utf-8"><label style="display: none;" for="email">Seu e-mail</label><input id="email-newsletter" name="email" type="email" placeholder="Qual é o seu e-mail?"><input type="hidden" name="list" value="5hbIBhgttipQeZXjMcG0jA"><input type="hidden" name="subform" value="yes"> <input type="submit" name="submit" id="submit-newsletter" value="Inscrever"></form>
-
-		<p>Contato: <a href="mailto:ghedin@manualdousuario.net">ghedin@manualdousuario.net</a></p>
-	</div>
-
 	<?php get_search_form(); ?>
 	
 	<p class="site-footer-footer">
--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Tags: featured-images, threaded-comments
 Requires at least: 4.5
 Tested up to: 8.1.12
 Requires PHP: 5.6
-Stable tag: 2.7
+Stable tag: 2.8
 License: GNU General Public License v2 or later
 License URI: LICENSE
 
@@ -40,6 +40,12 @@ More information: https://github.com/Automattic/_s
 Dez includes support for WooCommerce and for Infinite Scroll in Jetpack.
 
 == Changelog ==
+
+= 2.8 - February 23 2024 =
+* Altera logo e cabeçalho
+* Diminui margens entre elementos (leiaute mais denso)
+* Remove itens agora redundantes do rodapé
+* Altera cor do link “Continue lendo” (`<!--more-->`)
 
 = 2.7 - February 07 2024 =
 * Corrige alinhamento do botão “Apoie”
