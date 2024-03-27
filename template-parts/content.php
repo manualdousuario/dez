93d2d5ddcad9af3bd8afb8b0ac1bc81ed4baf1d3
Rodrigo Ghedin <rodrigo@ghed.in>
2024-03-27 08:05:19 -0300

    * Correção da chamada do littlefoot.js no `functions.php` * Inclusão de Markdown nos comentários via `functions.php` * Refatoração e simplificação do `style.css` * Novo leiaute para o cabeçalho em telas pequenas * Leves mudanças na página de erro (`404.php`) * Inclusão de novos links no rodapé * Leiaute simplificado das caixas de destaque (`ctx-*`) * Remoção da família de fontes sem serifa

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,25 +9,22 @@
 
 ?>
 
-<?php if ( is_home() ) : 
-	the_date('D j\/n\/Y', '<p class="data-capa">', '</p>');
-endif; ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<div class="entry-meta">
-			<?php if ( is_single() || is_archive() ) :
-				echo the_time( 'j/n/y, G\hi' );
-			elseif ( has_post_format( array('aside', 'quote', 'link') ) ) : 
-				echo '<a href="'. esc_url( get_permalink() ) .'" rel="bookmark" class="aside-link">';
-				echo get_the_time( 'G\hi' );
-				echo '</a>';
-			elseif ( is_home() ) :
-				echo get_the_time( 'G\hi' ); 
-			endif ?>
+		<div class="entry-meta link-alt">
+			<?php 
+				if ( has_post_format( array('aside', 'image', 'link', 'quote') ) ) :
+					echo get_the_time( 'j/n/y, ');
+					echo '<a href="'. esc_url( get_permalink() ) .'" rel="bookmark" class="link-alt">';
+					echo get_the_time( 'G\hi' );
+					echo '</a>';
+				elseif ( ! is_page() ) :
+					echo get_the_time( 'j/n/y, G\hi' );
+				endif ?>
+
 			<?php if ( comments_open() || get_comments_number() ) :
 				echo '&middot;&nbsp;';
-				comments_popup_link( '<span>0</span>', '<span>1</span>', '<span>%</span>', 'comment-link', '' );
+				comments_popup_link( '<span>0</span>', '<span>1</span>', '<span>%</span>', 'comment-link link-alt', '' );
 			endif; ?>
 			<?php if ( ( 'post' || 'podcast' === get_post_type() ) && ( ! in_category( array( 'post-livre', 'patrocinios' ) ) && ! has_tag( array( 'como-eu-trabalho', 'na-mochila', 'escritorio-em-casa' ) ) ) ) : ?>
 				<span class="author-<?php the_author_meta('ID'); ?>">&middot; por <?php echo get_the_author(); ?></span>
