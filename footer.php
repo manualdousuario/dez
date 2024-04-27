041e574c0397a9526693559d53ceab7f299ed75d
Rodrigo Ghedin <rodrigo@ghed.in>
2024-04-27 09:05:26 -0300

    Atualizações no rodapé

--- footer.php
+++ footer.php
@@ -14,11 +14,12 @@
 <footer id="colophon" class="site-footer">
 	<p>
 		<a href="mailto:ghedin@manualdousuario.net">Contato</a> &middot; <a href="/patrocine/">Patrocine</a></p>
-		<p>Associado à <a href="https://ajor.org.br/">Ajor</a> &middot; Apoio de <a href="https://www.teramundi.com/">Teramundi</a><br />
+		<p>Associado à <a href="https://ajor.org.br/">Ajor</a><br />
+			Apoio: <a href="https://buttondown.email/?utm_campaign=manual&amp;utm_source=footer">Buttondown</a> &middot; <a href="https://www.teramundi.com/">Teramundi</a><br />
 		2013–<?php echo esc_html( gmdate( 'Y' ) ); ?> &middot; <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt-br">CC BY-NC-SA 4.0</a>
 	</p>
 	<p>
-		<a href="https://wordpress.com/pt-br/refer-a-friend/36MJNNnS0q22qAPWJwYA/">WordPress.com</a> &middot; <a href="https://manualdousuario.net/?koko-analytics-dashboard=1">Audiência</a> &middot; <a href="https://status.manualdousuario.net">Status</a>
+		<a href="https://wordpress.com/pt-br/refer-a-friend/36MJNNnS0q22qAPWJwYA/">WordPress.com</a> &middot; <a href="https://app.pulsetic.com/status/zXGInCQ1">Status</a>
 	</p>
 </footer>
 </div><!-- #page -->
