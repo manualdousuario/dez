0d0db5936dccebe25f3973e81b203df84a5325db
Rodrigo Ghedin <rodrigo@ghed.in>
2026-02-02 19:03:33 -0300

    Corrige chamada da template-part content.php no page.php

--- page.php
+++ page.php
@@ -21,7 +21,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'content', 'page' );
 
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
