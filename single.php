bb4ffce09900955c97890f3fcc6e04d36da652d3
Rodrigo Ghedin <rodrigo@ghed.in>
2026-02-09 10:11:05 -0300

    Adiciona lista dos dez últimos textões no rodapé da capa e dos posts

--- single.php
+++ single.php
@@ -28,14 +28,30 @@ get_header();
 
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
-		endif;
+	endif;
 
 	endwhile; // End of the loop.
 
-	if ( shortcode_exists( 'sc' ) && $currentlang == 'pt-BR' ) :
-		echo do_shortcode( '[sc name="box-promocoes"][/sc]' ); 
+	$textoes_query = new WP_Query(array(
+		'tag_id' => 2259,
+		'posts_per_page' => 10,
+		'post__not_in' => array(get_the_ID())
+	));
+
+	if ($textoes_query->have_posts()) : ?>
+		<div class="ultimes-textoes" style="margin-top: var(--med-salto-grande); padding: 0 .5rem;">
+			<h2>Últimos textões</h2>
+			<ul>
+				<?php while ($textoes_query->have_posts()) : $textoes_query->the_post(); ?>
+					<li><a href="<?php the_permalink(); ?>?utm_campaign=interna&utm_content=textoes"><?php the_title(); ?></a></li>
+				<?php endwhile; ?>
+			</ul>
+		</div>
+		<?php 
+		wp_reset_postdata();
 	endif; ?>
+
 </main><!-- #main -->
 
-	<?php
-	get_footer();
+<?php
+get_footer();
